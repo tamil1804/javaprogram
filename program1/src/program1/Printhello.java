f1ec4fb591d7fe0120c839389f007b8a4488d2d2
tamil arasan <tamilsaravanan740@gmail.com>
2022-02-15 07:58:19 +0530

    program1 checkin

---
+++ program1/src/program1/Printhello.java
@@ -0,0 +1,14 @@
+package program1;
+
+public class Printhello
+{
+   public static void main(String arg[])
+   {
+	   int i;
+	   for(i=1;i<=5;i++)
+	   {
+		 System.out.println("HELLO");
+	   }
+	   
+   }
+}
