eac21451355118236448bcf40376f00786d0fe1f
tamil arasan <tamilsaravanan740@gmail.com>
2022-02-15 08:10:46 +0530

    even and odd

---
+++ program3/src/program3/evenodd.java
@@ -0,0 +1,18 @@
+package program3;
+
+public class evenodd
+{
+	public static void main(String arg[])
+	{
+		int a;
+		a=100;
+		if(a%2==0) 
+		{
+			System.out.println("The number is an Even number");
+		}
+		else 
+		{
+			System.out.println("The number is an Odd number");
+		}
+	}
+}
