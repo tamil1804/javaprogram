d8df567fb791f335c4399229016a4e9fcbb58b68
tamil arasan <tamilsaravanan740@gmail.com>
2022-02-20 08:00:18 +0530

    Sum of five numbers

---
+++ Sum of five no/src/sum/Myclass.java
@@ -0,0 +1,28 @@
+package sum;
+
+import java.util.Scanner;
+
+public class Myclass 
+{
+	public static void main(String arg[])
+	{
+		int[] data=new int[5];
+		Scanner obj =new Scanner(System.in);
+		int sum=0, avg;
+		
+		for(int i = 0;i<data.length;i++)
+		{
+	      System.out.println("Enter any Number");
+	      data[i]=obj.nextInt();
+		}
+			for(int d:data)
+			
+		      sum+=d;
+		      avg=sum/data.length;
+		      System.out.println("Sum of Five numbers :"+sum);
+		      System.out.println("Average of five number :"+avg);
+	
+	
+		    		
+	}
+}
